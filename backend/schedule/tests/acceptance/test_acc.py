24a55222e2680b85d1fd2afe03a887bdd8db8156
JokusPokus <jakob.schmitt@code.berlin>
2022-03-22 19:21:38 +0100

    test: Add dummy test cases

---
+++ backend/schedule/tests/acceptance/test_acc.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+pytestmark = pytest.mark.acceptance
+
+
+class TestPollSerializer:
+    def test_setup_works(self):
+        assert True
---
+++ backend/schedule/tests/integration/test_int.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+pytestmark = pytest.mark.integration
+
+
+class TestPollSerializer:
+    def test_setup_works(self):
+        assert True
