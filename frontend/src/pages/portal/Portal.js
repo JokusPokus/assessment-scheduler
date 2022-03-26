7f4136a37705e036088a8d7c0207ce71fedccefc
JokusPokus <jakob.schmitt@code.berlin>
2022-03-26 14:14:44 +0100

    ui: Improve portal styling

--- frontend/src/pages/portal/Portal.js
+++ frontend/src/pages/portal/Portal.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Layout } from 'antd';
+import './Portal.css'
 import SideBar from '../../components/sideBar/SideBar';
 
 const { Header, Content, Footer } = Layout;
