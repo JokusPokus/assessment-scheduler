a00deafc98c7ff46628aba9d6f6b98a0e8e7684e
JokusPokus <jakob.schmitt@code.berlin>
2022-03-29 18:40:11 +0200

    feat: Create user admin

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as OriginalUserAdmin
+from .models import User, Organization
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(OriginalUserAdmin):
+    list_display = ["id", "email", "date_joined", "organization"]
+    search_fields = ["email", "organization"]
+    ordering = ["id"]
+
+    fieldsets = (
+        *OriginalUserAdmin.fieldsets,
+        (None, {'fields': ['organization']},),
+    )
