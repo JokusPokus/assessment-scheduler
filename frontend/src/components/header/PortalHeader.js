1b8c901ded1eb40a3d622195248b6dd9c13e62ed
JokusPokus <jakob.schmitt@code.berlin>
2022-03-29 16:05:17 +0200

    ui: Extract PortalHeader component

---
+++ frontend/src/components/header/PortalHeader.js
@@ -0,0 +1,60 @@
+import { Button, Select, Layout } from "antd";
+import { Link } from "react-router-dom";
+import React from "react";
+
+const PortalHeader = ({ phase, setPhase, phases, setPhases, phaseData}) => {
+    const years = Object.keys(phaseData);
+
+    const handleYearChange = value => {
+        setPhases(phaseData[value]);
+        setPhase(phaseData[value][0]);
+    };
+
+    const onPhaseChange = value => {
+        setPhase(value);
+    };
+
+    const removeCookies = () => {
+        window.localStorage.removeItem('access');
+        window.localStorage.removeItem('refresh');
+    };
+
+    return (
+        <Layout.Header className="navbar-layout-background" >
+            <div className='navbar-content'>
+                <div className="phase-selection-group">
+                    <Button className='phases-button' type={'link'}>
+                        Assessment Phase:
+                    </Button>
+                    <Select
+                        defaultValue={years[0]}
+                        style={{ width: 120, marginLeft: "30px" }}
+                        onChange={handleYearChange}
+                        bordered={false}
+                    >
+                        {years.map(year => (
+                            <Select.Option key={year}>{year}</Select.Option>
+                        ))}
+                    </Select>
+                    <Select
+                        style={{ width: 240, marginLeft: "30px" }}
+                        value={phase}
+                        onChange={onPhaseChange}
+                        bordered={false}
+                    >
+                        {phases.map(assessPhase => (
+                            <Select.Option key={assessPhase}>{assessPhase}</Select.Option>
+                        ))}
+                    </Select>
+                </div>
+                <Link to='/login' style={{ textDecoration: 'none' }}>
+                    <Button className='logout-button' type="link" onClick={removeCookies}>
+                        Log Out
+                    </Button>
+                </Link>
+            </div>
+        </Layout.Header>
+    )
+};
+
+export default PortalHeader;
--- frontend/src/pages/portal/Portal.js
+++ frontend/src/pages/portal/Portal.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Layout, Select } from 'antd';
+import { Layout, Select } from 'antd';
 import './Portal.css'
 import SideBar from '../../components/sideBar/SideBar';
+import PortalHeader from "../../components/header/PortalHeader";
 
 const { Header, Content, Footer } = Layout;
 const { Option } = Select;
 
 const UserPortal = ({ requestUrl, refreshRequestBody }) => {
-    const phaseData = {
-        2022: ["Spring Semester", "Fall Semester"],
-        2021: ["Fall Semester"],
-    };
-    const years = Object.keys(phaseData);
-
     const [activeTab, setActiveTab] = useState('main');
+    const [activeTabComponent, setActiveTabComponent] = useState(
+        <h1>Welcome, user!</h1>
+    );
 
     const changeActiveTab = (tabName) => {
         return (event) => {
@@ -23,25 +21,10 @@ const UserPortal = ({ requestUrl, refreshRequestBody }) => {
         }
     };
 
-    const [activeTabComponent, setActiveTabComponent] = useState(
-        <h1>Welcome, user!</h1>
-    );
-
-    const [phases, setPhases] = useState(phaseData[years[0]]);
-    const [phase, setPhase] = useState(phaseData[years[0]][0]);
-
-    const handleYearChange = value => {
-        setPhases(phaseData[value]);
-        setPhase(phaseData[value][0]);
-    };
-
-    const onPhaseChange = value => {
-        setPhase(value);
-    };
-
     useEffect(() => {
         const tabs = {
             MAIN: "main",
+            SECOND: "second"
         };
 
         switch (activeTab) {
@@ -50,54 +33,36 @@ const UserPortal = ({ requestUrl, refreshRequestBody }) => {
                     <h1>Welcome, user!</h1>
                 );
                 break;
+            case tabs.SECOND:
+                setActiveTabComponent(
+                    <h1>This is another tab!</h1>
+                );
+                break;
         }
     }, [activeTab, requestUrl, refreshRequestBody ]);
 
-    const removeCookies = () => {
-        window.localStorage.removeItem('access');
-        window.localStorage.removeItem('refresh');
+    const phaseData = {
+        2022: ["Spring Semester", "Fall Semester"],
+        2021: ["Fall Semester"],
     };
+    const years = Object.keys(phaseData);
+
+    const [phases, setPhases] = useState(phaseData[years[0]]);
+    const [phase, setPhase] = useState(phaseData[years[0]][0]);
 
     return(
-        <Layout className="site-layout-background">
-            <SideBar changeActiveTab={changeActiveTab}></SideBar>
+        <Layout className="site-layout-background" style={{ minHeight: "100vh" }}>
+            <SideBar changeActiveTab={changeActiveTab}/>
             <Layout>
-                <Header className="navbar-layout-background" >
-                    <div className='navbar-content'>
-                        <div className="phase-selection-group">
-                            <Button className='phases-button' type={'link'}>
-                                Assessment Phase:
-                            </Button>
-                            <Select
-                                defaultValue={years[0]}
-                                style={{ width: 120, marginLeft: "30px" }}
-                                onChange={handleYearChange}
-                                bordered={false}
-                            >
-                                {years.map(year => (
-                                    <Option key={year}>{year}</Option>
-                                ))}
-                            </Select>
-                            <Select
-                                style={{ width: 240, marginLeft: "30px" }}
-                                value={phase}
-                                onChange={onPhaseChange}
-                                bordered={false}
-                            >
-                                {phases.map(assessPhase => (
-                                    <Option key={assessPhase}>{assessPhase}</Option>
-                                ))}
-                            </Select>
-                        </div>
-                        <Link to='/login' style={{ textDecoration: 'none' }}>
-                            <Button className='logout-button' type="link" onClick={removeCookies}>
-                                Log Out
-                            </Button>
-                        </Link>
-                    </div>
-                </Header>
+                <PortalHeader
+                    phase={phase}
+                    setPhase={setPhase}
+                    phases={phases}
+                    setPhases={setPhases}
+                    phaseData={phaseData}
+                />
                 <Content className='page-content'>
-                    <h1>Welcome!</h1>
+                    { activeTabComponent }
                 </Content>
             </Layout>
         </Layout>
