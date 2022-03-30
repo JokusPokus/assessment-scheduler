e3f292d96a5fecf078eaafc11abebfc35c1c2952
JokusPokus <jakob.schmitt@code.berlin>
2022-03-30 11:48:33 +0200

    feat: Create block model

--- backend/schedule/models.py
+++ backend/schedule/models.py
@@ -50,6 +50,9 @@ class AssessmentPhase(BaseModel):
 class Window(BaseModel):
     """An independent time range that can be scheduled 'in one go'. One or more
     windows make up an assessment phase.
+
+    Each window consists of one or more blocks, which in turn hosts a number
+    of assessments and breaks.
     """
 
     assessment_phase = models.ForeignKey(
@@ -59,8 +62,45 @@ class Window(BaseModel):
     )
     start_date = models.DateField()
     end_date = models.DateField()
+    block_length = models.IntegerField(
+        validators=[MinValueValidator(10), MaxValueValidator(420)]
+    )
 
     def clean(self):
         super().clean()
         if start_date > end_date:
             raise ValidationError('Start date after end date')
+
+
+class Block(BaseModel):
+    """A collection of back-to-back exams and breaks of the same assessor.
+
+    Each day within an assessment phase can contain a fixed number of blocks
+    that have a fixed total length. Assessments within a block have a fixed,
+    equal length. In turn, the length of assessments and breaks between
+    different blocks may vary.
+
+    E.g., the following 3-hour blocks are both feasible:
+
+    +-----------------+      +-----------------+
+    |   20 min exam   |      |   30 min exam   |
+    |   20 min exam   |      |   30 min exam   |
+    |   20 min exam   |      |   30 min exam   |
+    |   20 min break  |      |   30 min break  |
+    |   20 min exam   |      |   30 min exam   |
+    |   20 min exam   |      |   30 min exam   |
+    |   20 min break  |      +-----------------+
+    |   20 min exam   |
+    |   20 min exam   |
+    +-----------------+
+    """
+
+    window = models.ForeignKey(
+        'schedule.Window',
+        related_name='blocks',
+        on_delete=models.CASCADE
+    )
+    start_time = models.DateTimeField()
+    exam_length = models.IntegerField(
+        validators=[MinValueValidator(10), MaxValueValidator(360)]
+    )
