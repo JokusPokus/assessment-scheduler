a409520c129973c881b644821295f5c553af671d
JokusPokus <jakob.schmitt@code.berlin>
2022-03-30 16:05:24 +0200

    feat: Create viewset for planning sheet

--- backend/input/views.py
+++ backend/input/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import PlanningSheetSerializer
+
+
+class PlanningSheetView(CreateAPIView):
+    """View to receive the general CSV planning sheet and initiates its
+    processing.
+
+    * Requires token authentication
+    * Only Examination Office administrators can access this view
+    """
+    serializer_class = PlanningSheetSerializer
