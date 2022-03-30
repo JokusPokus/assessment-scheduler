1e9f8eae049f57ad6ccf766761696a1ff56f2b60
JokusPokus <jakob.schmitt@code.berlin>
2022-03-30 14:36:14 +0200

    feat: Create student model

--- backend/exam/models.py
+++ backend/exam/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from core.models import BaseModel
+
+
+class Student(BaseModel):
+    """A student who is to be assessed in a number of modules."""
+
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f'<Student: {self.email}>'
