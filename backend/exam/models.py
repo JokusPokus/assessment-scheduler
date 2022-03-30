c4a1d9969ebcc7131c413feb189fcdcf8122e581
JokusPokus <jakob.schmitt@code.berlin>
2022-03-30 14:57:01 +0200

    feat: Create model module

--- backend/exam/models.py
+++ backend/exam/models.py
@@ -10,3 +10,13 @@ class Student(BaseModel):
 
     def __str__(self):
         return f'<Student: {self.email}>'
+
+
+class Module(BaseModel):
+    """An academic unit that requires assessments."""
+
+    code = models.CharField(max_length=32)
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'<Module: {self.name}>'
