4e8947867e2564a590a0ec4500c27c2a3a0482fc
JokusPokus <jakob.schmitt@code.berlin>
2022-03-31 18:10:45 +0200

    feat: Add assessment phase view set

--- backend/schedule/views.py
+++ backend/schedule/views.py
@@ -1,1 +1,18 @@
-from django.shortcuts import render
+from rest_framework.mixins import CreateModelMixin
+from rest_framework.viewsets import ModelViewSet
+
+from .models import AssessmentPhase
+from .serializers import (
+    AssessmentPhaseDetailSerializer,
+    AssessmentPhaseListSerializer
+)
+
+
+class AssessmentPhaseViewSet(ModelViewSet):
+    def get_queryset(self):
+        """Return all assessment phases that belong to the requesting user's
+        organization.
+        """
+        return AssessmentPhase.objects.filter(
+            organization=self.request.user.organization
+        )
