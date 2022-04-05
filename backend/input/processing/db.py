f8005deaa64802e4da3c65478dcd9079dd36b9ee
JokusPokus <jakob.schmitt@code.berlin>
2022-04-05 19:10:14 +0200

    feat: Start db recorder class

---
+++ backend/input/processing/db.py
@@ -0,0 +1,12 @@
+class DBRecorder:
+    """Reads a CSV file and saves the contained information into the
+    database, using a normalized schema.
+    """
+
+    def __init__(self, file_path: str):
+        self.path = file_path
+
+    def write_to_db(self) -> None:
+        """Read all relevant information from the CSV file located by
+        :self.file_path: and save each line in a separate database record.
+        """
