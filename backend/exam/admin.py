e8d95b3f914c696db5770919b21f95a9a2206850
JokusPokus <jakob.schmitt@code.berlin>
2022-04-07 14:48:45 +0200

    feat: Display modules in admin

--- backend/exam/admin.py
+++ backend/exam/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Student
+from .models import Student, Module
 
 
 @admin.register(Student)
@@ -9,3 +9,11 @@ class StudentAdmin(admin.ModelAdmin):
         list_display = ['id', 'email']
         search_fields = ['email']
         ordering = ['email']
+
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    class Meta:
+        list_display = ['id', 'short_code', 'name']
+        search_fields = ['short_code', 'name']
+        ordering = ['short_code']
