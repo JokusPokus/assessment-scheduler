365c2939165f7bd004be35fe08e3346a3965a62a
JokusPokus <jakob.schmitt@code.berlin>
2022-04-08 18:49:30 +0200

    ui: Improve login page styling

--- frontend/src/pages/login/LogIn.js
+++ frontend/src/pages/login/LogIn.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Button, Layout, Form, Input, message } from "antd";
 import { useNavigate } from "react-router-dom";
 import { Content } from "antd/lib/layout/layout";
+import "./login.css"
 
 const { Header } = Layout;
 
@@ -54,17 +55,41 @@ const LogIn = ({ requestUrl, setUserInfo }) => {
     };
 
     return(
-        <Layout className="site-layout-background">
-            <Header className="header navbar-layout-background" >
-                <div className='navbar-content'>
-                </div>
-            </Header>
-            <Layout>
-                <Content style={{ width: '30%', margin: 'auto', padding: '50px 50px', background: '#fff'}}>
+        <Layout
+            className="site-layout-background"
+            style={{
+                height: "100%"
+            }}
+        >
+            <Layout
+                style={{
+                    backgroundImage: `url(/landing-page.jpg)`,
+                    backgroundSize: "cover",
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}
+            >
+                <Content style={{
+                    margin: 'auto',
+                    padding: '50px 50px',
+                    width: '30%',
+                    backgroundColor: 'white',
+                    borderRadius: '4px',
+                    boxShadow: 'rgba(0, 0, 0, 0.8) 12px 12px 12px',
+                    flex: '0 1 auto'
+                }}>
                     <div className='page_content'>
                         <div className='left_col'>
-                            <div className='greet'>
-                                <p className='sign_up'>Sign In to get started!</p>
+                            <div
+                                className='greet'
+                                style={{
+                                    marginTop: '10px',
+                                    marginBottom: '20px',
+                                    fontFamily: "'Source Sans Pro', sans-serif",
+                                    fontSize: '2rem'
+                                }}
+                            >
+                                <p className='sign_up'>CODE Assessment Scheduler</p>
                             </div>
                         </div>
                         <div className='right_col'>
@@ -78,7 +103,9 @@ const LogIn = ({ requestUrl, setUserInfo }) => {
                                 }}
                                 onFinish={onFinish}
                                 onFinishFailed={onFinishFailed}
-                                autoComplete="off" >
+                                autoComplete="on"
+                                size="large"
+                            >
 
                                 <Form.Item
                                     name="email"
