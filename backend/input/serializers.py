478697825f45f85346eb3be8389f854da6cbe0a3
JokusPokus <jakob.schmitt@code.berlin>
2022-04-12 15:18:50 +0200

    feat: Initiate sheet validation

--- backend/input/serializers.py
+++ backend/input/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
 from .models import PlanningSheet
+from .validation import SheetValidator
 
 
 class PlanningSheetSerializer(serializers.ModelSerializer):
     class Meta:
         model = PlanningSheet
         fields = ['window', 'csv']
+
+    @staticmethod
+    def validate_csv(file):
+        SheetValidator(file).validate()
+        return file
--- backend/input/validation.py
+++ backend/input/validation.py
@@ -1,10 +1,19 @@
 """
 Validation of CSV planning sheet.
 """
+import io
+
+import pandas as pd
 
 
 class SheetValidator:
     """Validates that all planning sheet requirements are met."""
 
-    def __init__(self, file_path: str):
-        self.path = file_path
+    def __init__(self, file: str):
+        self.file = file
+
+    def validate(self):
+        data = pd.read_csv(
+            io.StringIO(self.file.read().decode('utf-8')), delimiter=','
+        )
+        print(data)
