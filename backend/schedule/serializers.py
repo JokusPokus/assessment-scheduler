840d73dcf46f728c3b2e16f1d89e5bc1914d1818
JokusPokus <jakob.schmitt@code.berlin>
2022-04-22 16:26:26 +0200

    fix: Improve datetime output format

--- backend/schedule/serializers.py
+++ backend/schedule/serializers.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from django.utils.timezone import make_aware
 from rest_framework import serializers
@@ -29,14 +29,13 @@ class BlockSlotSerializer(serializers.ModelSerializer):
 
     @staticmethod
     def get_date_portion(obj):
-        return obj.start_time.day
+        return obj.start_time.strftime("%Y-%m-%d")
 
     @staticmethod
     def get_time_portion(obj):
-        return obj.start_time.strftime("HH-MM")
+        return obj.start_time.strftime("%H:%M")
 
-    @staticmethod
-    def to_internal_value(data):
+    def to_internal_value(self, data):
         """Combine the date and time of a block slot to a proper datetime
         object.
         """
@@ -44,8 +43,29 @@ class BlockSlotSerializer(serializers.ModelSerializer):
         time = datetime.strptime(data.pop('time'), '%H:%M').time()
 
         start_time = datetime.combine(date, time)
+        data['start_time'] = make_aware(start_time)
 
-        return {**data, 'start_time': make_aware(start_time)}
+        self._validate_time(data)
+
+        return data
+
+    @staticmethod
+    def _validate_time(data):
+        """Validate that there is no other block slot interfering with
+        the current one.
+
+        If at least one other block slot too early or not early enough
+        to avoid conflict, raise a ValidationError.
+        """
+        window = data.get('window')
+        min_distance = timedelta(minutes=window.block_length)
+        start_times = window.block_slots.all().values_list('start_time', flat=True)
+
+        for time in start_times:
+            if abs(time - data.get('start_time')) < min_distance:
+                raise serializers.ValidationError(
+                    {'start_time': 'Overlapping block slots'}
+                )
 
 
 class WindowSerializer(serializers.ModelSerializer):
