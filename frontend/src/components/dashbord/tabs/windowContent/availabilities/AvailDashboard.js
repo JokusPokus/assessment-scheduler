12ee47710d6336bb625c5ef283fa1a145fbac103
JokusPokus <jakob.schmitt@code.berlin>
2022-04-24 21:58:41 +0200

    ui: Avail dashboard checkpoint

--- frontend/src/components/dashbord/tabs/windowContent/availabilities/AvailDashboard.js
+++ frontend/src/components/dashbord/tabs/windowContent/availabilities/AvailDashboard.js
@@ -1,9 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import getDaysArray from "../../../../../utils/datetime";
-import {Table} from "antd";
+import {Table, Tag} from "antd";
 import {httpGetAssessors} from "../../../../../hooks/requests";
+import {foldSlotData} from "../../../../../utils/slots";
 
 const _ = require('lodash');
+const {CheckableTag} = Tag;
 
 const assessorColumn = [
     {
@@ -22,7 +24,52 @@ const helperColumn = [
 ];
 
 
+const AvailChecks = ({day, assessor, availableTimes, availData, setAvailData}) => {
+    const [selectedTags, setSelectedTags] = useState([]);
+
+    const handleChange = (tag, checked) => {
+        const nextSelectedTags = checked ? [...selectedTags, tag] : selectedTags.filter(t => t !== tag);
+        setSelectedTags(nextSelectedTags);
+    };
+
+    useEffect(() => {
+        console.log("USED", availData);
+        const nextAssessorAvails = {
+            [assessor]: {
+                ...(assessor in availData ? availData[assessor] : {}),
+                [day]: selectedTags
+            }
+        };
+        console.log({...availData, ...nextAssessorAvails});
+        setAvailData({...availData, ...nextAssessorAvails});
+    }, [selectedTags]);
+
+    useEffect(() => {
+        console.log(availData)
+    }, [availData]);
+
+    return (
+        <>
+            {availableTimes.map(tag => (
+                <CheckableTag
+                    key={tag}
+                    checked={selectedTags.indexOf(tag) > -1}
+                    onChange={checked => handleChange(tag, checked)}
+                    style={{
+                        padding: "8px",
+                        fontSize: "1.1em"
+                    }}
+                >
+                    {tag}
+                </CheckableTag>
+            ))}
+        </>
+    )
+};
+
+
 const AvailDashboard = ({window}) => {
+    console.log("YUPPU");
     const days = getDaysArray(window.start_date, window.end_date);
     const daysColumns = days.map(day => (
         {
@@ -33,13 +80,47 @@ const AvailDashboard = ({window}) => {
     ));
 
     const [assessors, setAssessors] = useState([]);
+    const [assTableSource, setAssTableSource] = useState(null);
+    const [availData, setAvailData] = useState({});
 
-    useEffect(async() => {
+    const slotData = foldSlotData(window.block_slots);
+
+    useEffect(() => {
+        console.log(availData)
+    }, [availData]);
+
+    useEffect(async () => {
         const response = await httpGetAssessors(window.id)();
         let assess = await response.json();
-        assess.sort();
-        setAssessors((assess).map(ass => ass.email))
+        let assessEmails = assess.map(ass => ass.email);
+        assessEmails.sort();
+        setAssessors(assessEmails);
     }, []);
+
+    useEffect(() => {
+        const newAssTableSource = assessors && assessors.map(ass => {
+            let dayElements = days.map(day => (
+                {
+                    [day]: <AvailChecks
+                        key={day}
+                        day={day}
+                        assessor={ass}
+                        availableTimes={slotData[day]}
+                        availData={availData}
+                        setAvailData={setAvailData}
+                    />
+                }
+            ));
+            return Object.assign(
+                {
+                    assessor: ass,
+                    key: ass
+                },
+                ...dayElements
+            );
+        });
+        setAssTableSource(newAssTableSource)
+    }, [assessors]);
 
     return (
         <>
@@ -47,18 +128,18 @@ const AvailDashboard = ({window}) => {
                 Assessors
             </h1>
             <Table
-                dataSource={assessors && assessors.map(ass => ({assessor: ass}))}
+                dataSource={assTableSource}
                 columns={[...assessorColumn, ...daysColumns]}
                 pagination={false}
             />
             <h1 style={{textAlign: 'left', marginTop: '80px', marginLeft: '10px'}}>
                 Helpers
             </h1>
-            <Table
+            {/*<Table
                 dataSource={null}
                 columns={[...helperColumn, ...daysColumns]}
                 pagination={false}
-            />
+            />*/}
         </>
     )
 };
