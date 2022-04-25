555e5e44f85b7593dc76ceb56a5070333032e4ff
JokusPokus <jakob.schmitt@code.berlin>
2022-04-25 13:34:48 +0200

    feat: Expose availabilities via assessor endpoint

--- backend/staff/serializers.py
+++ backend/staff/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from .models import Assessor
+from schedule.serializers import BlockSlotSerializer
 
 
 class AssessorSerializer(serializers.ModelSerializer):
+    available_slots = BlockSlotSerializer(read_only=True, many=True)
+
     class Meta:
         model = Assessor
-        fields = ['email']
+        fields = ['email', 'available_slots']
