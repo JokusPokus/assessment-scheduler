0083ba6afbd46656077866aea088800eb63795b4
JokusPokus <jakob.schmitt@code.berlin>
2022-05-03 15:25:24 +0200

    feat: Define class to collect input from db

--- backend/schedule/scheduling/input_collectors.py
+++ backend/schedule/scheduling/input_collectors.py
@@ -6,6 +6,10 @@ from dataclasses import dataclass
 
 from django.db.models import QuerySet
 
+from schedule.models import Window, BlockSlot, BlockTemplate
+from exam.models import Exam, Module, Student
+from staff.models import Assessor, Helper
+
 
 class BaseInputCollector(ABC):
     """Defines an interface for input collector classes."""
@@ -25,6 +29,24 @@ class InputData:
     modules: QuerySet
     assessors: QuerySet
     helpers: QuerySet
-    students: QuerySet
     block_slots: QuerySet
     block_templates: QuerySet
+
+
+class DBInputCollector(BaseInputCollector):
+    """Collects input data from the database used by
+    the Django project.
+    """
+
+    def __init__(self, window):
+        self.window = window
+
+    def collect(self) -> InputData:
+        return InputData(
+            exams=Exam.objects.filter(window=self.window),
+            modules=Module.objects.filter(windows=self.window),
+            assessors=Assessor.objects.filter(windows=self.window),
+            helpers=Helper.objects.filter(windows=self.window),
+            block_slots=BlockSlot.objects.filter(window=self.window),
+            block_templates=BlockTemplate.objects.filter(windows=self.window)
+        )
