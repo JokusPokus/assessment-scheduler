17d7df536b5e0aa4b515ba2413540f368499615e
JokusPokus <jakob.schmitt@code.berlin>
2022-05-03 16:18:53 +0200

    feat: Define basic algorithm interface

---
+++ backend/schedule/scheduling/algorithms.py
@@ -0,0 +1,25 @@
+"""
+Implementations of planning algorithms to solve the exam scheduling
+problem.
+"""
+from abc import ABC, abstractmethod
+
+from .input_collectors import InputData
+
+
+class Schedule:
+    pass
+
+
+class BaseAlgorithm(ABC):
+    """Defines an interface for Algorithm implementations."""
+
+    def __init__(self, data: InputData):
+        self.data = data
+
+    @abstractmethod
+    def run(self) -> Schedule:
+        """Solve the exam scheduling problem as defined by self.data and
+        return the solution as a Schedule instance.
+        """
+        pass
