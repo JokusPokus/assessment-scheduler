3a2fcbf87ddb42056a39d31c3ef6251d881a79d3
JokusPokus <jakob.schmitt@code.berlin>
2022-05-11 12:02:29 +0200

    test: Add test case

--- backend/schedule/scheduling/tests/unit/test_input_helper_classes.py
+++ backend/schedule/scheduling/tests/unit/test_input_helper_classes.py
@@ -25,3 +25,19 @@ class TestAvailInfo:
         # ASSERT
         assert avail_info.assessor_count == 1
         assert avail_info.assessors == [assessor_2]
+
+    def test_removing_non_existing_assessor_throws_error(self, assessor_mock):
+        # ARRANGE
+        assessor_1 = assessor_mock()
+        assessor_2 = assessor_mock()
+
+        avail_info = AvailInfo(
+            assessor_count=1,
+            assessors=[assessor_1],
+            helper_count=0,
+            helpers=[]
+        )
+
+        # ACT / ASSERT
+        with pytest.raises(ValueError):
+            avail_info.remove(assessor_2)
