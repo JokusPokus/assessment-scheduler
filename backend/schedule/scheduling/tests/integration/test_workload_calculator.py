22c1778106007c03d6f56ee5104f7e29befcd085
JokusPokus <jakob.schmitt@code.berlin>
2022-05-13 00:22:55 +0200

    test: Add test case

--- backend/schedule/scheduling/tests/integration/test_workload_calculator.py
+++ backend/schedule/scheduling/tests/integration/test_workload_calculator.py
@@ -23,7 +23,6 @@ class TestWorkloadCalculator:
     ):
         # ARRANGE
         window = create_window()
-
         ass = create_assessor(window=window)
 
         module_20_30 = create_modules(
@@ -75,10 +74,14 @@ class TestWorkloadCalculator:
 
         # ASSERT
         expected_exams_20 = 6 + 1
-        num_exams_per_block_20 = 7
+        num_exams_per_block_20 = len(
+            BlockTemplate.objects.get(exam_length=20).exam_start_times
+        )
 
         expected_exams_30 = 4 + 5
-        num_exams_per_block_30 = 5
+        num_exams_per_block_30 = len(
+            BlockTemplate.objects.get(exam_length=30).exam_start_times
+        )
 
         expected = {
             20: math.ceil(expected_exams_20 / num_exams_per_block_20),
@@ -86,3 +89,56 @@ class TestWorkloadCalculator:
         }
 
         assert workload == {ass: expected}
+
+    def test_other_assessors_do_not_interfere(
+            self,
+            create_assessor,
+            create_modules,
+            create_exams,
+            create_window,
+    ):
+        # ARRANGE
+        window = create_window()
+        ass = create_assessor(window=window)
+        other_ass = create_assessor(window=window)
+
+        module_20_30 = create_modules(
+            n=1,
+            standard_length=20,
+            alternative_length=30
+        )
+
+        create_exams(
+            n=7,
+            style=ExamStyle.STANDARD,
+            assessor=ass,
+            module=module_20_30,
+        )
+        create_exams(
+            n=7,
+            style=ExamStyle.STANDARD,
+            assessor=other_ass,
+            module=module_20_30,
+        )
+
+        calc = WorkloadCalculator(
+            assessors=Assessor.objects.filter(id__in=[ass.id, other_ass.id]),
+            exams=Exam.objects.filter(assessor__in=[ass.id, other_ass.id]),
+            block_templates=BlockTemplate.objects.filter(
+                exam_length__in=[20, 30]
+            ),
+        )
+
+        # ACT
+        workload = calc.assessor_block_counts
+
+        # ASSERT
+        expected_exams_20 = 7
+        num_exams_per_block_20 = len(
+            BlockTemplate.objects.get(exam_length=20).exam_start_times
+        )
+
+        assert len(workload) == 2
+        assert workload[ass] == {
+            20: math.ceil(expected_exams_20 / num_exams_per_block_20)
+        }
