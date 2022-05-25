125f1f1fb7ec535be49e0cdeb8688d583613e374
JokusPokus <jakob.schmitt@code.berlin>
2022-05-25 16:45:37 +0200

    feat: Integrate db schedules in output writer

--- backend/schedule/scheduling/output_writers.py
+++ backend/schedule/scheduling/output_writers.py
@@ -2,45 +2,53 @@
 Conversion of in-memory schedule to useful output formats
 """
 from exam.models import Exam
-from schedule.models import BlockSlot, Block, ExamSlot, BlockTemplate
+from schedule.models import (
+    Window,
+    Block,
+    BlockSlot,
+    BlockTemplate,
+    ExamSlot,
+    Schedule as DBSchedule,
+)
 
 from .schedule import Schedule, BlockSchedule, ExamSchedule
 
 
 class DBOutputWriter:
     """Writes a Schedule instance to a normalized database representation."""
-    def __init__(self, schedule: Schedule):
+    def __init__(self, window: Window, schedule: Schedule):
+        self.window = window
         self.schedule = schedule
 
     def write_to_db(self) -> None:
+        db_schedule = DBSchedule.objects.create(window=self.window)
+
         for slot_id, block_schedules in self.schedule.items():
             slot = BlockSlot.objects.get(id=slot_id)
 
             for block_schedule in block_schedules:
                 block = Block.objects.create(
+                    schedule=db_schedule,
                     slot=slot,
-                    template=self._get_template_from(block_schedule, slot)
+                    template=self._get_template_from(block_schedule)
                 )
                 for exam_schedule in block_schedule.exams:
                     self._create_and_link_exam_slot(
                         block,
                         block_schedule,
                         exam_schedule,
-                        slot
                     )
 
-    @staticmethod
-    def _get_template_from(block_schedule, slot) -> BlockTemplate:
-        return slot.window.block_templates.get(
+    def _get_template_from(self, block_schedule) -> BlockTemplate:
+        return self.window.block_templates.get(
             exam_length=block_schedule.exam_length
         )
 
-    @staticmethod
     def _create_and_link_exam_slot(
+            self,
             block: Block,
             block_schedule: BlockSchedule,
             exam_schedule: ExamSchedule,
-            slot: BlockSlot
     ) -> None:
         exam_slot = ExamSlot.objects.create(
             block=block,
@@ -48,7 +56,7 @@ class DBOutputWriter:
         )
         exam = Exam.objects.get(
             code=exam_schedule.exam_code,
-            window=slot.window
+            window=self.window
         )
         exam.time_slot = exam_slot
         exam.helper = block_schedule.helper
