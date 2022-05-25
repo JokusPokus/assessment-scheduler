035df48aab9df01f9e37d4025f59dd958592eec4
JokusPokus <jakob.schmitt@code.berlin>
2022-05-25 21:36:45 +0200

    feat: Enhance planning sheet admin inline

--- backend/input/admin.py
+++ backend/input/admin.py
@@ -4,6 +4,7 @@ from .models import PlanningSheet
 
 class PlanningSheetInline(admin.TabularInline):
     model = PlanningSheet
-    fields = ['csv']
+    readonly_fields = ['is_filled_out']
+    fields = ['csv', 'is_filled_out']
     show_change_link = True
     extra = 0
