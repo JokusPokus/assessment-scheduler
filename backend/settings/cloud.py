d34ef229d2339f14c475b498e352f0fe61f702a4
JokusPokus <jakob.schmitt@code.berlin>
2022-06-09 10:54:54 +0200

    cloud: Fix static root name

--- backend/settings/cloud.py
+++ backend/settings/cloud.py
@@ -64,7 +64,9 @@ DEFAULT_FILE_STORAGE = 'core.storage.backends.GoogleCloudStaticStorage'
 STATICFILES_STORAGE = 'core.storage.backends.GoogleCloudStaticStorage'
 GS_STATIC_BUCKET_NAME = 'examsched-media'
 STATIC_URL = 'https://storage.googleapis.com/{}/'.format(GS_STATIC_BUCKET_NAME)
-# STATIC_ROOT = 'static/'
+STATIC_ROOT = 'static/'
+
+MEDIA_URL = 'https://storage.googleapis.com/{}/'.format(GS_STATIC_BUCKET_NAME)
 
 
 # Enable Django security precautions
